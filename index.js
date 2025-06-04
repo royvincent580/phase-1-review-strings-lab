2dade1fe88bdf34c4b2651175155bba2551b61db
royvincent580 <vincentroy580@gmail.com>
2025-06-05 00:02:23 +0300

    strings

--- index.js
+++ index.js
@@ -3,6 +3,4 @@ const currentUser = 'Grace Hopper';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = 'Welcome, ';
-const shortGreeting = `Welcome, ${currentUser}`;
 const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
